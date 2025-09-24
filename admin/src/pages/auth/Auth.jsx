9cdd95413625a0cdef27eb753778e1196e5645d2
Izzatbek <abdusharipovizzat03@gmail.com>
2025-09-24 14:37:18 +0500

    fashion commerce

--- admin/src/pages/auth/Auth.jsx
+++ admin/src/pages/auth/Auth.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./Auth.css"
-import { Mail } from 'lucide-react';
+import { Lock, Mail, Send } from 'lucide-react';
 
 const Auth = () => {
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
   return (
     <>
     <div className="w-full h-screen bg_image">
@@ -9,9 +12,29 @@ const Auth = () => {
         <div className="bg-black text-white w-full max-w-md p-6 rounded-md shadow-md">
           <h1 className="text_color text-3xl font-semibold pb-4">Sign In</h1>
           <form className="space-y-4">
-            <div className="">
+            <div className="form_input border p-3 rounded-md">
               <Mail size={20} />
-              <input type="email" name="email" id="email" placeholder="Email" />
+              <input type="email" name="email" id="email"
+               placeholder="Email"
+               className="w-full outline-none bg-transparent"
+               value={email}
+               onChange={(e)=>setEmail(e.target.value)}
+               required
+               />
+            </div>
+            <div className="form_input border p-3 rounded-md">
+              <Lock size={20} />
+              <input type="password" name="password" id="password"
+              placeholder="********"
+              className="outline-none w-full bg-transparent"
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+              required
+               />
+            </div>
+            <div className="flex items-center justify-center gap-3 bg_color p-3 rounded-md shadow-md cursor-pointer">
+              <Send size={20} />
+              <input type="submit" value="Sign In" />
             </div>
           </form>
         </div>
