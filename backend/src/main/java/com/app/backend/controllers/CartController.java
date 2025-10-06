987e4458a1f14acd292a92cd75e0c3d92767eed9
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-06 15:59:33 +0500

    commerce

--- backend/src/main/java/com/app/backend/controllers/CartController.java
+++ backend/src/main/java/com/app/backend/controllers/CartController.java
@@ -1,4 +1,13 @@
 package com.app.backend.controllers;
 
+import com.app.backend.services.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/carts")
+@RequiredArgsConstructor
 public class CartController {
+    private final CartService cartService;
 }
